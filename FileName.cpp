0a28a8b402d3f54c6a3562ef9147474744fdef44
Abdullah Soufan <abdullah.t.s.soufan@gmail.com>
2025-10-28 18:41:19 +0300

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	// this program finds the sum of two numbers
+	int x, y, sum;
+	cout << "Enter the first number(x): ";
+	cin >> x;
+
+	cout << " Enter the second number(y): ";
+	cin >> y;
+		sum = x + y;
+	cout << "The sum is " << sum;
+	return 0;
+
+}

19db03d8e780515d3554ca6c7d0d6afe8d4b243c
Abdullah Soufan <abdullah.t.s.soufan@gmail.com>
2025-10-28 19:38:57 +0300

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "Hwo much is you masroof" << endl;
-	int int5, int10, inttotal;
-	int5 = 5;
-	int10 = 10;
-	inttotal = int5 + int10;
-
-	cout << "The total is: " << inttotal << endl;
-
+	// this program computes students age and prints out his name and favourite color
+	string Firstname, lastname;
+	int name;
+	int age;
+	int year; // year of birth
+	string color;
+	cout << " please enter your first name and last name:\n ";
+	cin >> Firstname;
+	cin >> lastname;
+	cout << " please enter your year of birth:\n";
+	cin >> year;
+	cout << " please enter your favourite color:\n";
+	cin >> color;
+	//compute students age
+	age = 2025 - year;
+	cout <<" name \t\t age \t\t color\n";
+		cout << Firstname << "\t" << age << "\t\t"<<color << endl;
 	return 0;
 }

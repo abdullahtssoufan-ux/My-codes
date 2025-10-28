9cfcc741a88abad21f366b6576e27ed3b45ed8e6
Abdullah Soufan <abdullah.t.s.soufan@gmail.com>
2025-10-28 18:43:00 +0300

    Add files via upload

---
+++ abood.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num1;
+
+	cout << " What is your name ? "<< endl;
+	cout << "Abdullah Tariq Saeed Soufan";
+	cin >> num1;
+
+
+	return 0;
+}
